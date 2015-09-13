22dcb841ba75b6f65c121a40546b8fa298aca20e
shankster36 <hellophoto116@gmail.com>
2015-09-12 20:30:13 -0500

    cleanup and stateui

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', []).
-config(['$routeProvider', function($routeProvider) {
-  //$routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('myApp', ['ui.router']).
+config(function($stateProvider, $routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/'});
+  $stateProvider
+    .state('searchForCitation', {
+      url: "/",
+      templateUrl: "pages/SearchForCitation/searchForCitation.html",
+      controller: "SearchForCitation"
+    })
+    .state('citationsList', {
+      url: "/listCitation",
+      templateUrl: "app/directives/citationPanel/citationPanel.html",
+      controller: "citationPanel"
+    });
+});
